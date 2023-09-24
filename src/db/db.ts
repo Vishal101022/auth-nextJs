f73d2b59e9f746320a5e2747f8afa789b2625735
Vishal101022 <raivishal939@gmail.com>
2023-09-24 17:46:14 +0530

    :card_file_box: db connection

---
+++ src/db/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_DB_URL!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("Database connected");
+    });
+    connection.on("error", (error) => {
+      console.log("MongoDB connection error: ", error);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+  }
+}
